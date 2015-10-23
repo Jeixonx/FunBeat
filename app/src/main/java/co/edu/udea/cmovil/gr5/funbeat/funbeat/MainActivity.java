2ab4881b9362c1a9bab6771280bad663d45234d4
Jeixonx <jeixon@ideashock.net>
2015-10-23 13:31:41 -0500

    buffer arreglado

--- app/src/main/java/co/edu/udea/cmovil/gr5/funbeat/funbeat/MainActivity.java
+++ app/src/main/java/co/edu/udea/cmovil/gr5/funbeat/funbeat/MainActivity.java
@@ -3,21 +3,18 @@ package co.edu.udea.cmovil.gr5.funbeat.funbeat;
 import android.content.Context;
 import android.media.AudioManager;
 import android.media.SoundPool;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.GestureDetector;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
-import java.util.*;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
-import static java.util.concurrent.TimeUnit.*;
+
+import java.util.HashMap;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 
@@ -36,10 +33,11 @@ public class MainActivity extends AppCompatActivity {
 
     //ultima vez que hizo tick el metronomo
     long ultimotiempo = 0;
+    long penultimoTiempo = 0;
 
 
     int velocidad;
-    int buffer = 500;
+    int buffer;
 
 
     @Override
@@ -62,16 +60,20 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        //definicion logica de los botones
+        //definicion logica de la gui
         Button boton1 = (Button) findViewById(R.id.button);
         final Button boton2 = (Button) findViewById(R.id.button2);
-        final Button boton3 = (Button) findViewById(R.id.button3);
+        final Button botonMetronomo = (Button) findViewById(R.id.button3);
+        final Button botonBuffer = (Button) findViewById(R.id.button4);
         final TextView muestratiempo = (TextView) findViewById(R.id.textView);
         final TextView tiempoPresion = (TextView) findViewById(R.id.textView2);
         final TextView tiempoMin = (TextView) findViewById(R.id.textView3);
         final TextView tiempoMax = (TextView) findViewById(R.id.textView4);
         final ImageView personaje = (ImageView) findViewById(R.id.imageView);
+        final EditText editBuffer = (EditText) findViewById(R.id.editText2);
 
+
+        buffer = Integer.parseInt(editBuffer.getText().toString());
 
 
         boton1.setOnTouchListener(new View.OnTouchListener() {
@@ -87,15 +89,19 @@ public class MainActivity extends AppCompatActivity {
                         mSoundPool.stop(0);
                         mStream2 = mSoundPool.play((Integer) mSoundPoolMap.get(SOUND_FX_02), streamVolume, streamVolume, 1, LOOP_1_TIME, 1f);
 
-                        tiempoMin.setText(""+ (ultimotiempo+velocidad-buffer));
-                        tiempoPresion.setText(""+System.currentTimeMillis());
-                        tiempoMax.setText(""+(ultimotiempo+velocidad+buffer));
-                        if (System.currentTimeMillis() > ultimotiempo+velocidad-buffer &&  System.currentTimeMillis() < ultimotiempo+velocidad+buffer ){
+                        tiempoMin.setText("" + (ultimotiempo + velocidad - buffer));
+                        tiempoPresion.setText("" + System.currentTimeMillis());
+                        tiempoMax.setText("" + (ultimotiempo + velocidad + buffer));
+                        if (System.currentTimeMillis() > ultimotiempo + velocidad - buffer && System.currentTimeMillis() < ultimotiempo + velocidad + buffer) {
                             //acierto
                             personaje.setImageResource(R.drawable.icono);
-                        }else{
-                            //fallo
-                            personaje.setImageResource(R.drawable.iconotriste);
+                        } else {
+                            if (System.currentTimeMillis() > penultimoTiempo + velocidad - buffer && System.currentTimeMillis() < penultimoTiempo + velocidad + buffer) {
+                                personaje.setImageResource(R.drawable.icono);
+                            }else {
+                                //fallo
+                                personaje.setImageResource(R.drawable.iconotriste);
+                            }
                         }
 
 
@@ -123,70 +129,93 @@ public class MainActivity extends AppCompatActivity {
                         mStream2 = mSoundPool.play((Integer) mSoundPoolMap.get(SOUND_FX_01), streamVolume, streamVolume, 1, LOOP_1_TIME, 1f);
 
                         tiempoMin.setText("" + (ultimotiempo + velocidad - buffer));
-                        tiempoPresion.setText(""+System.currentTimeMillis());
-                        tiempoMax.setText(""+(ultimotiempo+velocidad+buffer));
-                        if (System.currentTimeMillis() > ultimotiempo+velocidad-buffer &&  System.currentTimeMillis() < ultimotiempo+velocidad+buffer ){
+                        tiempoPresion.setText("" + System.currentTimeMillis());
+                        tiempoMax.setText("" + (ultimotiempo + velocidad + buffer));
+                        if (System.currentTimeMillis() > ultimotiempo + velocidad - buffer && System.currentTimeMillis() < ultimotiempo + velocidad + buffer) {
                             //acierto
                             personaje.setImageResource(R.drawable.icono2);
-                        }else{
-                            //fallo
-                            personaje.setImageResource(R.drawable.iconotriste);
+                        } else {
+                            if (System.currentTimeMillis() > penultimoTiempo + velocidad - buffer && System.currentTimeMillis() < penultimoTiempo + velocidad + buffer) {
+                                personaje.setImageResource(R.drawable.icono2);
+                            }else {
+                                //fallo
+                                personaje.setImageResource(R.drawable.iconotriste);
+                            }
+                            }
+                            return true; // if you want to handle the touch event
+                            case MotionEvent.ACTION_UP:
+                                // RELEASED
+
+                                return true; // if you want to handle the touch event
                         }
-                        return true; // if you want to handle the touch event
-                    case MotionEvent.ACTION_UP:
-                        // RELEASED
-
-                        return true; // if you want to handle the touch event
+                        return false;
                 }
-                return false;
-            }
-        });
-
-        boton3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                if (!isTimerOn) {
-
-                    EditText velo = (EditText) findViewById(R.id.editText);
-                    //para cambiar la velocidad
-                    velocidad = Integer.parseInt(velo.getText().toString());
-
-
-                    timer[0] = new Timer();
-                    final TimerTask myTask = new TimerTask() {
-                        @Override
-                        public void run() {
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    //aqui cada segundo
-                                    float streamVolume = mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
-                                    streamVolume = streamVolume / mAudioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
-
-                                    mSoundPool.stop(0);
-                                    mStream2 = mSoundPool.play((Integer) mSoundPoolMap.get(3), streamVolume, streamVolume, 1, LOOP_1_TIME, 1f);
-
-                                    ultimotiempo = System.currentTimeMillis();
-                                    muestratiempo.setText("" + ultimotiempo);
-                                }
-                            });
-                        }
-                    };
-                    timer[0].scheduleAtFixedRate(myTask, 1000, velocidad);
-                    //timer.schedule(myTask, 1000, 1000);
-                    isTimerOn = true;
-                } else {
-                    timer[0].cancel();
-                    isTimerOn = false;
-                }
-
             }
 
-        });
+            );
 
+            botonMetronomo.setOnClickListener(new View.OnClickListener()
+
+                                              {
+                                                  @Override
+                                                  public void onClick(View v) {
+
+                                                      if (!isTimerOn) {
+
+                                                          EditText velo = (EditText) findViewById(R.id.editText);
+                                                          //para cambiar la velocidad
+                                                          velocidad = Integer.parseInt(velo.getText().toString());
+
+
+                                                          timer[0] = new Timer();
+                                                          final TimerTask myTask = new TimerTask() {
+                                                              @Override
+                                                              public void run() {
+                                                                  runOnUiThread(new Runnable() {
+                                                                      @Override
+                                                                      public void run() {
+                                                                          //aqui cada segundo
+                                                                          float streamVolume = mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+                                                                          streamVolume = streamVolume / mAudioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+
+                                                                          mSoundPool.stop(0);
+                                                                          mStream2 = mSoundPool.play((Integer) mSoundPoolMap.get(3), streamVolume, streamVolume, 1, LOOP_1_TIME, 1f);
+
+                                                                          penultimoTiempo = ultimotiempo;
+                                                                          ultimotiempo = System.currentTimeMillis();
+                                                                          muestratiempo.setText("" + ultimotiempo);
+                                                                      }
+                                                                  });
+                                                              }
+                                                          };
+                                                          timer[0].scheduleAtFixedRate(myTask, 1000, velocidad);
+                                                          //timer.schedule(myTask, 1000, 1000);
+                                                          isTimerOn = true;
+                                                      } else {
+                                                          timer[0].cancel();
+                                                          isTimerOn = false;
+                                                      }
+
+                                                  }
+
+                                              }
+
+            );
+
+            botonBuffer.setOnClickListener(new View.OnClickListener()
+
+                                           {
+                                               @Override
+                                               public void onClick(View v) {
+                                                   buffer = Integer.parseInt(editBuffer.getText().toString());
+                                               }
+
+                                           }
+
+            );
+
+
+        }
 
     }
-
-}
 
