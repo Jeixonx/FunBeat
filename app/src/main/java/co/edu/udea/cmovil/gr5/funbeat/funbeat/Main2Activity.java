3045e4f418071042e12e3884a618b2e8fb392b6b
Jeison <jeixon@ideashock.net>
2015-11-13 01:36:15 -0500

    Animaciones y drag and drop del personaje.

---
+++ app/src/main/java/co/edu/udea/cmovil/gr5/funbeat/funbeat/Main2Activity.java
@@ -0,0 +1,13 @@
+package co.edu.udea.cmovil.gr5.funbeat.funbeat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
